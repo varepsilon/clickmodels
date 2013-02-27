22515179ddca8e78bd06fcc7b45db8d24c8cde7e
Aleksandr Chuklin <chuklin@yandex-team.ru>
2013-02-27 13:47:59 +0400

    Revert "Test significance of changes on further pages."
    It gets killed because of high memory consumption :(
    
    This reverts commit 86fc28ba4e97c743b7815d88809ffdd9c10b5284.

--- testSignificance.py
+++ testSignificance.py
@@ -2,14 +2,13 @@
 
 import sys
 import glob
-import gc
 
 from inference import *
 from bootstrap import bootstrap
 
 
 def perpGain(r1, r2):
-    return (r1 - r2) / ((r1 - 1) if r1 != 1 else 0.000001)
+    return (r1 - r2) / (r1 - 1)
 
 
 def llGain(r1, r2):
@@ -25,74 +24,57 @@ def avg(l):
     return float(s) / n if n else 0
 
 
+# TESTED_MODEL_PAIRS = ['UBM', 'EB_UBM', 'EB_UBM-IA']
+TESTED_MODEL_PAIRS = ['UBM']
+
+if 'RBP' in TESTED_MODEL_PAIRS:
+    import scipy
+    import scipy.optimize
+
+MODEL_CONSTRUCTORS = {
+    'DBN': (lambda: DbnModel((0.9, 0.9, 0.9, 0.9)), lambda: DbnModel((1.0, 0.9, 1.0, 0.9), ignoreIntents=False, ignoreLayout=False)),
+    'UBMvsDBN': (UbmModel, lambda: DbnModel((0.9, 0.9, 0.9, 0.9))),
+    'UBM': (UbmModel, lambda: UbmModel(ignoreIntents=False, ignoreLayout=False)),
+    'EB_UBM': (UbmModel, EbUbmModel, lambda: EbUbmModel(ignoreIntents=False, ignoreLayout=False)),
+    'DCM': (DcmModel, lambda: DcmModel(ignoreIntents=False, ignoreLayout=False)),
+    'RBP': (SimplifiedRbpModel, lambda: RbpModel(ignoreIntents=False, ignoreLayout=False))
+}
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print >>sys.stderr, 'Usage: {0:s} directory_with_files'.format(sys.argv[0])
         sys.exit(1)
-    modelName = 'SDBN'
-    TESTED_MODEL_PAIRS = [modelName]
     perplexityGains = dict((m, defaultdict(lambda: [])) for m in TESTED_MODEL_PAIRS)
     perplexityGainsPos = [dict((m, defaultdict(lambda: [])) for m in TESTED_MODEL_PAIRS) for pos in xrange(MAX_NUM)]
     llGains = dict((m, defaultdict(lambda: [])) for m in TESTED_MODEL_PAIRS)
     interestingFiles = sorted(glob.glob(sys.argv[1] + '/*'))
-    N = len(interestingFiles) - 1
+    N = len(interestingFiles) // 2
     for fileNumber in xrange(N):
-        trainFile = interestingFiles[fileNumber]
-        testFile = interestingFiles[fileNumber + 1]
+        trainFile = interestingFiles[2 * fileNumber]
+        testFile = interestingFiles[2 * fileNumber + 1]
         readInput = InputReader()
-        res = []
-        m = SimplifiedDbnModel()
-        trainFileObj = open(trainFile)
-        trainSessions = readInput(trainFileObj)
-        trainFileObj.close()
-        print 'train read'
-        testFileObj = open(testFile)
-        # Only needed to compute MAX_QUERY_ID
-        readInput(testFileObj)
-        testFileObj.close()
-        print 'test read'
-        m.train(trainSessions)
-        del trainSessions
-        gc.collect()
-        print 'model trained'
-        testFileObj = open(testFile)
-        testSessions = readInput(testFileObj)
-        testFileObj.close()
-        currentResult = m.test(testSessions)
-        del testSessions
-        gc.collect()
-        res.append(currentResult)
-        print >>sys.stderr, float(fileNumber) / N, modelName, currentResult
-        del m
-        gc.collect()
-        m = SimplifiedDbnModel()
-        trainFileObj = open(trainFile)
-        trainSessions = readInput(trainFileObj)
-        trainSessions = [InputReader.mergeExtraToSessionItem(s) for s in trainSessions]
-        trainFileObj.close()
-        m.train(trainSessions)
-        del trainSessions
-        gc.collect()
-        testFileObj = open(testFile)
-        testSessions = readInput(testFileObj)
-        testSessions = [InputReader.mergeExtraToSessionItem(s) for s in testSessions]
-        testFileObj.close()
-        currentResult = m.test(testSessions)
-        del testSessions
-        del m
-        gc.collect()
-        res.append(currentResult)
-        print >>sys.stderr, float(fileNumber) / N, modelName, currentResult
-        i = 0
-        j = 1
-        perplexityGains[modelName][(i, j)].append(perpGain(res[i][1], res[j][1]))
-        llGains[modelName][(i, j)].append(llGain(res[i][0], res[j][0]))
-        for pos in xrange(MAX_NUM):
-            perplexityGainsPos[pos][modelName][(i, j)].append(perpGain(res[i][2][pos], res[j][2][pos]))
+        trainSessions = readInput(open(trainFile))
+        testSessions = readInput(open(testFile))
+        for modelName in TESTED_MODEL_PAIRS:
+            res = []
+            models = MODEL_CONSTRUCTORS[modelName]
+            for idx, model in enumerate(models):
+                m = model()
+                m.train(trainSessions)
+                currentResult = m.test(testSessions, reportPositionPerplexity=True)
+                res.append(currentResult)
+                print >>sys.stderr, float(fileNumber) / N, modelName, idx, currentResult
+                del m
+            for i in xrange(len(models)):
+                for j in xrange(i + 1, len(models)):
+                    perplexityGains[modelName][(i, j)].append(perpGain(res[i][1], res[j][1]))
+                    llGains[modelName][(i, j)].append(llGain(res[i][0], res[j][0]))
+                    for pos in xrange(MAX_NUM):
+                        perplexityGainsPos[pos][modelName][(i, j)].append(perpGain(res[i][2][pos], res[j][2][pos]))
 
-    for t in ['perplexity']:
+    for t in ['ll', 'perplexity']:
         print t.upper()
-        for m in [modelName]:
+        for m in TESTED_MODEL_PAIRS:
             gainsDict = locals()[t + 'Gains'][m]
             for k, gains in gainsDict.iteritems():
                 print m, k, gains, bootstrap(gains)[1]
