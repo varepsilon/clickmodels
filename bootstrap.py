8b8203464ab967155dfd13c65a95005f872852f9
Aleksandr Chuklin <a.chuklin@gmail.com>
2014-04-05 18:14:31 +0200

    Remove a dependency from a third-party code.

--- bootstrap.py
+++ bootstrap.py
@@ -4,26 +4,26 @@ File    bootstrap.py
 
 Original Author  Ernesto P. Adorio, PhD.
         UPDEPP(U.P. at Clarkfield)
-Version 0.0.1 Sep. 25, 2010 # basic
-        0.0.2 Oct   9, 2011 # with bias output, smoothing(sigma >0)
+
+        http://adorio-research.org/wordpress/?p=12295
 """
+
+import numpy as np
 import random
-import quantile #http://adorio-research.org/wordpress/?p=125
+
 
 def mean(X):
     return sum(X) / float(len(X))
 
-def bootstrap(sample, samplesize=None, nsamples=1000, statfunc=mean, sigma=None, conf=0.95):
+def bootstrap(sample, samplesize=None, nsamples=1000, statfunc=mean, conf=0.95):
     """
     Arguments:
        sample - input sample of values
-       nsamples - number of samples to generate
        samplesize - sample size of each generated sample
+       nsamples - number of samples to generate
        statfunc- statistical function to apply to each generated sample.
-       sigma - if not None, resmple valuew will have an added normal component
-               with zero mean and sd sigma.
 
-    Returns bootstrap sample of statistic(computed by statfunc), bias and confidence interal.
+    Returns: bias (deviation of bootstrap from the sample) and confidence interal.
     """
     if samplesize is None:
         samplesize = len(sample)
@@ -31,20 +31,18 @@ def bootstrap(sample, samplesize=None, nsamples=1000, statfunc=mean, sigma=None,
     X = []
     for i in range(nsamples):
         resample = [random.choice(sample) for i in range(n)]
-        if sigma and sigma > 0:  # apply smoothing?
-           nnormals = scipy.normal.rvs(n, 0, sigma)
-           resample = [x + z for x, z in zip (resample, nnormals)]
         x = statfunc(resample)
         X.append(x)
-    bias = sum(X) / float(nsamples) - statfunc(sample)
+    bias = mean(X) - statfunc(sample)
 
-    plower  = (1 - conf)/2.0
+    plower  = (1 - conf) / 2.0
     pupper  = 1 - plower
-    symconf = (quantile.quantile(X, plower), quantile.quantile(X, pupper))
+    symconf = (np.percentile(X, plower * 100), np.percentile(X, pupper * 100))
     return bias, symconf
 
 
 def Test():
+    random.seed(42)
     x = [-0.0802915429072737, -0.053250352200284734, -0.01736675618732031, 0.023235301004853604, 0.11978171918496372, 0.05658880725170068, -0.009028639702335362, 0.044647677002185304, 0.11031884561909511, 0.0681469067959517, 0.005213181783769727, 0.05017491558203524, 0.01865825757134676, -0.040601203753332094, -0.016567084581231573]
     print bootstrap(x)
 
