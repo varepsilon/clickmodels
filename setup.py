9569c6ccd0ae8dfe769d70db6d10eac35fdad177
Artem Grotov <agrotov@gmail.com>
2014-05-27 19:54:14 +0200

    packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+      name='ClickModels',
+      version='0.1.0',
+      author='Aleksandr Chuklin',
+      author_email='A.Chuklin@uva.nl',
+      packages=['ClickModels'],
+      license='LICENSE.txt',
+      description='Useful click models.',
+      long_description=open('README.txt').read(),
+      install_requires=[
+                        ],
+      )
